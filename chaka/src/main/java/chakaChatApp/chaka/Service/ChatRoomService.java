392717a69e2025d70957d4b8921ddd252d02e79e
bill hiep <billhiepnguyen@gmail.com>
2024-06-11 07:22:24 +0700

    Create Service

---
+++ chaka/src/main/java/chakaChatApp/chaka/Service/ChatRoomService.java
@@ -0,0 +1,14 @@
+package chakaChatApp.chaka.Service;
+
+import chakaChatApp.chaka.Entity.ChatRoom;
+
+import java.util.List;
+import java.util.Optional;
+
+ public  interface ChatRoomService {
+    List<ChatRoom> getAllChatRooms();
+    Optional<ChatRoom> getChatRoomById(Long roomId);
+    ChatRoom createChatRoom(ChatRoom chatRoom);
+    ChatRoom updateChatRoom(Long roomId, ChatRoom chatRoomDetails);
+    void deleteChatRoom(Long roomId);
+}
